2c544e3995b91ffced52e802ff9cb54b9af2d1fc
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-25 00:55:29 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
-import React from 'react'
+import React , {useState} from 'react'
+
+import {Navbar} from "./components/Navbar"
+import {Footer} from "./components/Footer"
+import {Routes} from "./components/Routes"
 
 
 function App() {
+  const [darkTheme, setDarkTheme]=useState(false)
   return (
-    <div className="App">
+    <div className={darkTheme ? 'dark' : ''}>
       <div className="bg-gray-100">shezi</div>
       
     </div>

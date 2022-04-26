09f7739c62f91756a6d67f4093643af015fb6772
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-27 00:28:09 +0500

    work complete

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,8 @@
+import {react, createContext, useContext, useState} from 'react'
+
+const ResultContext=createContext()
+const baseUrl='https://google-search3.p.rapidapi.com/api/v1'
+
+export const ResultContextProvider=()={
+    
+}

737113f1c0bf6883b0e92ddbdd96fcad045eb11f
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-26 23:43:57 +0500

    work complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
-    
-      <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center border-b darkTheme:border-gray-700 border-gray-200">
-    <div classname="flex jusitfy-betweeen items-center space-x-5 w-screen "></div>
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center border-b darkTheme:border-gray-700 border-gray-200">
+      <div className="flex justify-between items-center space-x-5 w-screen ">
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 darkTheme:bg-gray-500 darkTheme: text-gray-900">
+            Googly 🔎 
+          </p>
+        </Link>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

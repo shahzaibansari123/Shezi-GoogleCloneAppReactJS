e443836ca1e0be43f8d9275e0829764edc56a39a
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-27 22:42:32 +0500

    work complete

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+
+import {useResultContext} from '../contexts/ResultContextProvider'
 
 const Results = () => {
+  const {results, isLoading,getResults,searchTerm} = useResultContext()
+  const location=useLocation()
   return (
     <div>Results</div>
   )
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,8 +1,41 @@
-import {react, createContext, useContext, useState} from 'react'
+import {React, createContext, useContext, useState} from 'React'
 
 const ResultContext=createContext()
 const baseUrl='https://google-search3.p.rapidapi.com/api/v1'
 
-export const ResultContextProvider=()={
-    
+export const ResultContextProvider=({children})=>{
+
+    const [results, setResults]=useState([])
+    const[isLoading, setIsLoading]=useState(false)
+    const [searchTerm,setSearchTerm]=useState('')
+
+    //search /images/ videos type prop
+   const getResults= async (type)=>{
+       setIsLoading(true)
+
+       const response =await fetch (`${baseUrl}${type}`,{
+           method: GET,
+           headers:{
+            'X-User-Agent': 'desktop',
+            'X-Proxy-Location': 'EU',
+            'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+            'X-RapidAPI-Key': '1c03fdca9bmshd8f34550b6b62c3p1d939ejsnb8f28c1718e9'
+           }
+       })
+
+       const data= await response.json()
+       setResults(data)
+       setIsLoading(false)
+
+   } 
+
+   return(
+       <ResultContext.Provider value={{getResults, results, searchTerm, setSearchTerm, isLoading}}>
+           {children}
+       </ResultContext.Provider>
+   )
+
+
 }
+
+export const useResultContext=()=> useContext(ResultContext)

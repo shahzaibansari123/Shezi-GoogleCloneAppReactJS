1da57793c842135dd987c8d3fdd546d590b02997
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-30 00:26:53 +0500

    work complete

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,29 +1,3 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom";
-
-// const links = [
-//   { url: "/search", text: "🔎 All" },
-//   { url: "/news", text: "📰 News" },
-//   { url: "/image", text: "📸 Images" },
-//   { url: "/videos", text: "📺 Videos" },
-// ];
-
-// const Links = () => {
-//   return (
-//     <div className="flex sm:justify-around justify-between items-center mt-4">
-//       {links.map(({url, text}) => (
-//         <NavLink
-//           to={url}
-//           activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"
-//         >
-//           {text}
-//         </NavLink>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Links;
 
 
 import React from 'react';
@@ -38,8 +12,8 @@ const links = [
 
 export const Links = () => (
   <div className="flex sm:justify-around justify-between items-center mt-4">
-    {links.map(({ url, text }) => (
-      <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
+    {links.map(({ url, text }, index) => (
+      <NavLink className="m-2 mb-0" key={index}  to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
     ))}
   </div>
 )
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ResultContextProvider>
-    <Router>
+    <Router forceRefresh>
     <App />
     </Router>
     </ResultContextProvider>
